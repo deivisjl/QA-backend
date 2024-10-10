148895bb531694d604ea29effa0de0370692a722
deivisjl <deivisjl@gmail.com>
2024-10-10 17:56:24 -0600

    docker compose config
    docker compose config

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,29 @@ const config = require('../config/app')
 exports.auth = (req, res, next) =>{
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-
+    
     if(!token){
         return res.status(401).json({error:'No autorizado'})
     }
 
-    jwt.verify(token, config.appKey,(err, user)=>{
-        if(err){
-            return res.status(401).json({error: err})
-        }
+    try
+    {
+        jwt.verify(token, config.appKey,(err, user)=>{
+            console.log('hola mundo: ' + err + user)
+            if(err){
+                console.log('hola mundo2: ' + err + user)
+                return res.status(401).json({error: err})
+            }
+            
+            req.user = user
+        }) 
 
-        req.user = user
-    }) 
+        console.log('hola mundo3: ' + err + user)
 
-    next()
+        next()
+    }
+    catch(err)
+    {
+        return res.status(401).json({error: err})
+    }
 }
